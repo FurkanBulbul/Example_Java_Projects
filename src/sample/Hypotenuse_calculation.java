5a742228636d04ac12d6b8634519bec70c478bd7
Furkan Bülbül <furkanbulbul532@gmail.com>
2023-10-12 13:32:06 +0300

    Hypotenuse_calculation
    Hypotenuse_calculation added

---
+++ src/sample/Hypotenuse_calculation.java
@@ -0,0 +1,28 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Hypotenuse_calculation {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the 1st side");
+        int a=scan.nextInt();
+
+        System.out.print("Enter the 2st side");
+        int b = scan.nextInt();
+
+        double c = Math.sqrt((a*a)+ (b*b));
+
+        System.out.println("hypotenuse-->"+ " "+ (int) c);
+
+
+
+
+
+
+
+
+    }
+}
